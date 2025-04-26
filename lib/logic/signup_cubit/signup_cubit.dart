aceb984a9e9bb7397f7322be87ea7a339d30dbf8
rami ashraf <eng.ramiashraf90@gmail.com>
2025-04-26 20:44:25 +0300

    updated

---
+++ lib/logic/signup_cubit/signup_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:doctor_app/logic/signup_cubit/signup_state.dart';
+import 'package:meta/meta.dart';
+
+class SignUpCubit extends Cubit<SignUpStates> {
+  SignUpCubit() : super(SignUpInitialStates());
+
+  Dio request = Dio();
+
+  Future<void> signUp({
+    required String email,
+    required String name,
+    required String gender,
+    required String phone,
+    required String pass,
+    required String confirmationPass,
+  }) async {
+    emit(SignUpLoadingState());
+    try {
+      await request.post(
+        "https://vcare.integration25.com/api/auth/register",
+        data: {
+          "name": name,
+          "email": email,
+          "phone": phone,
+          "gender": gender,
+          "password": pass,
+          "password_confirmation": confirmationPass,
+        },
+      );
+      emit(SignUpSucessState());
+    } catch (e) {
+      emit(SignUpErrorState(e.toString()));
+    }
+  }
+}
